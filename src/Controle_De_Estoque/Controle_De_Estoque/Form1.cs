93499aade159d7dbe9642a6930021a34b8b3f547
PA <pedro.augusto@inventsoftware.com.br>
2023-11-01 13:05:20 -0300

    Lista de produtosTapecaria implementada. DataGrid criada e populada com com itens da lista.

--- src/Controle_De_Estoque/Controle_De_Estoque/Form1.cs
+++ src/Controle_De_Estoque/Controle_De_Estoque/Form1.cs
@@ -15,6 +15,20 @@ namespace Controle_De_Estoque
         public Form1()
         {
             InitializeComponent();
+
+            List<ProdutoTapecaria> listaProdutoTapecaria = new List<ProdutoTapecaria>();
+
+            listaProdutoTapecaria.Add(new ProdutoTapecaria() {Tipo = TipoTapecaria.tapete, DataEntrada = new DateTime()});
+            listaProdutoTapecaria.Add(new ProdutoTapecaria() {Tipo = TipoTapecaria.cortina, DataEntrada = new DateTime() });
+            listaProdutoTapecaria.Add(new ProdutoTapecaria() {Tipo = TipoTapecaria.estofado, DataEntrada = new DateTime() });
+
+           /* for (var i = 0; i < listaProdutoTapecaria.Count; i++)
+            {
+                listaProdutoTapecaria[i].Id = i+1;
+            }*/
+
+            dataGridView1.DataSource = listaProdutoTapecaria;
         }
+
     }
 }
