15cd0c722471538ce716026b2a0a3a4810a81181
PA <pedro.augusto@inventsoftware.com.br>
2023-12-08 10:42:57 -0300

    Padronização nomenclatura

--- src/Controle_De_Estoque/Program.cs
+++ src/Controle_De_Estoque/Program.cs
@@ -7,57 +7,35 @@ namespace Controle_De_Estoque
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            //FluentMigrator
             using (var serviceProvider = CreateServices())
             using (var scope = serviceProvider.CreateScope())
             {
-                // Put the database update into a scope to ensure
-                // that all resources will be disposed.
                 UpdateDatabase(scope.ServiceProvider);
             }
 
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new TelaInicial());
         }
 
-        /// <summary>
-        /// Configure the dependency injection services
-        /// </summary>
         private static ServiceProvider CreateServices()
         {
             return new ServiceCollection()
-                // Add common FluentMigrator services
                 .AddFluentMigratorCore()
                 .ConfigureRunner(rb => rb
-                    // Add SQL Server support to FluentMigrator
                     .AddSqlServer()
-                    // Set the connection string
                     .WithGlobalConnectionString(ConfigurationManager.ConnectionStrings["SQL_Server_Controle_De_Estoque"].ConnectionString)
-                    // Define the assembly containing the migrations
                     .ScanIn(typeof(_20231207105700_AdicionarTabelaTapecaria).Assembly).For.Migrations())
-                // Enable logging to console in the FluentMigrator way
                 .AddLogging(lb => lb.AddFluentMigratorConsole())
-                // Build the service provider
                 .BuildServiceProvider(false);
         }
 
-        /// <summary>
-        /// Update the database
-        /// </summary>
         private static void UpdateDatabase(IServiceProvider serviceProvider)
         {
-            // Instantiate the runner
             var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
 
-            // Execute the migrations
             runner.MigrateUp();
         }
     }
