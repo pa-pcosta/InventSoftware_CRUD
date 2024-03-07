91bc2e8106dd9762b5068c7e5cb1f46b3c2dc32a
PA <pedro.augusto@inventsoftware.com.br>
2024-03-07 11:56:07 -0300

    Implementados métodos para validar campos. Implementada função disparapelo botão salvar

--- src/InterfaceSAPUI5/wwwroot/controller/CadastroProdutoTapecaria.controller.js
+++ src/InterfaceSAPUI5/wwwroot/controller/CadastroProdutoTapecaria.controller.js
@@ -27,25 +27,88 @@ sap.ui.define([
         },
 
         aoClicarEmSalvar (){
-            
-			var novoProdutoTapecaria = this.getView().getModel("produtoTapecaria").getData();
+            let listaDeErrosValidacao = Validador.validarTodos(this.getView());
 			
-			novoProdutoTapecaria.tipo = parseInt(novoProdutoTapecaria.tipo);
-			
-			fetch('api/Tapecaria', {
-				method: 'POST',
-				body: JSON.stringify(novoProdutoTapecaria),
-				headers: {
-					"Content-type": "application/json; charset=UTF-8"
-				}
-			})
-			.then(resposta => resposta.json())
-			.then(
-				produtoCadastrado =>
-					this.getOwnerComponent().getRouter().navTo("detalhes",{
-						id: produtoCadastrado.id
-					})
-			);
-        }
+			if(listaDeErrosValidacao.length == 0){
+				let novoProdutoTapecaria = this.getView().getModel("produtoTapecaria").getData();
+				
+				novoProdutoTapecaria.tipo = parseInt(novoProdutoTapecaria.tipo);
+				
+				fetch('api/Tapecaria', {
+					method: 'POST',
+					body: JSON.stringify(novoProdutoTapecaria),
+					headers: {
+						"Content-type": "application/json; charset=UTF-8"
+					}
+				})
+				.then(resposta => resposta.json())
+				.then(this.mostrarMessageBoxSucesso("SUCESSO !"))
+				.then(
+					produtoCadastrado =>
+						this.getOwnerComponent().getRouter().navTo("detalhes",{
+							id: produtoCadastrado.id
+						})
+				);
+			}
+			else
+			{
+				this.mostrarMessageBoxFalha("Alguns campos não atendem os critérios de validação")
+			}
+        },
+
+		aoMudarValorTipo (evento){
+			let comboBox = evento.getSource();
+			let mensagemErroValidacao = Validador.validarTipo(comboBox);
+
+			if (mensagemErroValidacao)
+			{
+				comboBox.setValueState("Error");
+                comboBox.setValueStateText(mensagemErroValidacao);
+			}
+		},
+
+		aoMudarValorDataEntrada (evento){
+			let datePicker = evento.getSource();
+			let mensagemErroValidacao = Validador.validarDataEntrada(datePicker);
+
+			if (mensagemErroValidacao)
+			{
+				datePicker.setValueState("Error");
+                datePicker.setValueStateText(mensagemErroValidacao);
+			}
+		},
+
+		aoMudarValorTamanho (evento){
+            let campoInput = evento.getSource();
+			let mensagemErroValidacao = Validador.validarTamanho(campoInput);
+
+			if (mensagemErroValidacao)
+			{
+				campoInput.setValueState("Error");
+                campoInput.setValueStateText(mensagemErroValidacao);
+			}
+		},
+
+		aoMudarValorPrecoMetroQuadrado (evento){
+            let campoInput = evento.getSource();
+			let mensagemErroValidacao = Validador.validarPrecoMetroQuadrado(campoInput);
+
+			if (mensagemErroValidacao)
+			{
+				campoInput.setValueState("Error");
+                campoInput.setValueStateText(mensagemErroValidacao);
+			}
+		},
+
+		aoMudarValorDetalhes (evento){
+            let campoInput = evento.getSource();
+			let mensagemErroValidacao = Validador.validarDetalhes(campoInput);
+
+			if (mensagemErroValidacao)
+			{
+				campoInput.setValueState("Error");
+                campoInput.setValueStateText(mensagemErroValidacao);
+			}
+		},
 	});
 });
--- src/InterfaceSAPUI5/wwwroot/services/validacaoCadastro.js
+++ src/InterfaceSAPUI5/wwwroot/services/validacaoCadastro.js
@@ -4,66 +4,106 @@ sap.ui.define([], () => {
 	
     return {
 
-        validarTipo(evento) {
-            let comboBox = evento.getSource();
+        validarTodos (view){
+            _listaDeErros = [];
+
+            let comboBox = view.byId("cadastroComboBoxTipo");
+            let datePicker = view.byId("cadastroDatePickerDataEntrada");
+            let inputArea = view.byId("cadastroInputArea");
+            let inputPreco = view.byId("cadastroInputPrecoMetroQuadrado");
+            let inputDetalhes = view.byId("cadastroInputDetalhes");
+
+            this.validarTipo(comboBox);
+            this.validarDataEntrada(datePicker);
+            this.validarTamanho(inputArea);
+            this.validarPrecoMetroQuadrado(inputPreco);
+            this.validarDetalhes(inputDetalhes);
+
+            return _listaDeErros;
+        },
+
+        validarTipo(comboBox) {
             let valorComboBox = comboBox.getSelectedKey();
 
-            if(valorComboBox==""){
-                _listaDeErros.erroTipo = "O produto precisa ter um tipo";
+            if(valorComboBox=="")
+            {
+                _listaDeErros.push("O produto precisa ter um tipo");
                 comboBox.setValueState("Error");
                 comboBox.setValueStateText("O produto precisa ter um tipo");
             }
+            else
+            {
+                comboBox.setValueState("None");
+            }
         },
 
-        validarDataEntrada(evento) {
-            let datePicker = evento.getSource();
+        validarDataEntrada(datePicker) {
             let valorDatePicker = datePicker.getDateValue();
             let hoje = new Date();
             
-            if (valorDatePicker > hoje)
+            if (valorDatePicker == null)
+            {
+                _listaDeErros.push("Insira uma data");
+                datePicker.setValueState("Error");
+                datePicker.setValueStateText("Insira uma data");
+            }
+            else if (valorDatePicker > hoje)
             {
                 _listaDeErros.push("A data de registro não pode ser maior do que a atual");
                 datePicker.setValueState("Error");
                 datePicker.setValueStateText("A data de registro não pode ser maior do que a atual");
             }
+            else
+            {
+                datePicker.setValueState("None");
+            }
         },
 
-        validarTamanho(evento) {
-            let campoInput = evento.getSource();
-            let valorCampoInput = campoInput.getValue();
-            let possuiApenasNumeros = /^\d+$/.test(valorCampoInput);
+        validarTamanho(inputArea) {
+            let valorInputArea = inputArea.getValue();
+            let possuiApenasNumeros = /^\d+$/.test(valorInputArea);
 
             if (!possuiApenasNumeros)
             {
                 _listaDeErros.push("Este campo aceita apenas números");
-                campoInput.setValueState("Error");
-                campoInput.setValueStateText("Este campo aceita apenas números");
+                inputArea.setValueState("Error");
+                inputArea.setValueStateText("Este campo aceita apenas números");
+            }
+            else
+            {
+                inputArea.setValueState("None");
             }
         },
 
-        validarPreco(evento) {
-            let campoInput = evento.getSource();
-            let valorCampoInput = campoInput.getValue();
-            let possuiApenasNumeros = /^\d+$/.test(valorCampoInput);
+        validarPrecoMetroQuadrado(inputPreco) {
+            let valorInputPreco = inputPreco.getValue();
+            let possuiApenasNumeros = /^\d+$/.test(valorInputPreco);
 
             if (!possuiApenasNumeros)
             {
                 _listaDeErros.push("Este campo aceita apenas números");
-                campoInput.setValueState("Error");
-                campoInput.setValueStateText("Este campo aceita apenas números");
+                inputPreco.setValueState("Error");
+                inputPreco.setValueStateText("Este campo aceita apenas números");
+            }
+            else
+            {
+                inputPreco.setValueState("None");
             }
         },
 
-        validarDetalhes(evento) {
-            let campoInput = evento.getSource();
-            let tamanhoCampoInput = campoInput.getValue().length;
-            let maximoDeCaracteres = 20;
+        validarDetalhes(inputDetalhes) {
+            let tamanhoInputDetalhes = inputDetalhes.getValue().length;
+            let maximoDeCaracteres = 50;
 
-            if (tamanhoCampoInput > maximoDeCaracteres)
+            if (tamanhoInputDetalhes > maximoDeCaracteres)
             {
                 _listaDeErros.push(`Digite no máximo ${maximoDeCaracteres} caracteres`);
-                campoInput.setValueState("Error");
-                campoInput.setValueStateText(`Digite no máximo ${maximoDeCaracteres} caracteres`);
+                inputDetalhes.setValueState("Error");
+                inputDetalhes.setValueStateText(`Digite no máximo ${maximoDeCaracteres} caracteres`);
+            }
+            else
+            {
+                inputDetalhes.setValueState("None");
             }
         }
 	};
